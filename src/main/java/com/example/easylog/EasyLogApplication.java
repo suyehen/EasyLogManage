be0617bfb4abafe89a61a0eaec152d22c08faf85
suyehen <hjx999999999@qq.com>
2025-05-05 12:57:43 +0800

    测试提交

---
+++ src/main/java/com/example/easylog/EasyLogApplication.java
@@ -0,0 +1,14 @@
+package com.example.easylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyLogApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyLogApplication.class, args);
+    }
+
+}
